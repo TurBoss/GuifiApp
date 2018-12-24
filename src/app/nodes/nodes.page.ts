3ec549dda3444e94ae5042ecb3b74b75729e5910
TurBoss <j.l.toledano.l@gmail.com>
2018-12-24 04:58:46 +0100

    some tuning

---
+++ src/app/nodes/nodes.page.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-nodes',
+  templateUrl: './nodes.page.html',
+  styleUrls: ['./nodes.page.scss'],
+})
+export class NodesPage implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
